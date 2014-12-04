1496f02e5e29a5f59d4feef23bdd069bbcab7ba8
paulbaker3 <paul.baker.3@gmail.com>
2014-12-04 00:43:17 -0700

    Basic logic

--- lib/log_it.rb
+++ lib/log_it.rb
@@ -1,5 +1,24 @@
 require "log_it/version"
 
 module LogIt
-  # Your code goes here...
+  def logit(stmnt, args={})
+    formatted_stmnt = args[:color].nil? ? stmnt : stmnt.colorize(args[:color])
+    lvl = args[:level] || args[:lvl] || args[:severity]
+
+    case lvl
+    when :unknown, 'unknown', 5, '5'
+      Rails.logger.unknown { formatted_stmnt }
+    when :fatal, 'fatal', 4, '4'
+      Rails.logger.fatal { formatted_stmnt }
+    when :error, 'error', 3, '3'
+      Rails.logger.error { formatted_stmnt }
+    when :warn, 'warn', 2, '2'
+      Rails.logger.warn { formatted_stmnt }
+    when :info, 'info', 1, '1'
+      Rails.logger.info { formatted_stmnt }
+    when :debug, 'debug', 0, '0', nil
+      Rails.logger.debug { formatted_stmnt }
+    end
+  end
+  method_alias :log_it, :logit
 end
